ccaa1acd081f27239f2cc8ad3c571bd0a9923f73
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-01-26 20:27:08 +0900

    #3.5 Detail Screen

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { connect } from "react-redux";
 import { actionCreators } from "../store";
+import { Link } from "react-router-dom";
 
-function ToDo({ text, onBtnClick }) {
+function ToDo({ text, onBtnClick, id }) {
   return (
     <li>
-      {text} <button onClick={onBtnClick}>DEL</button>
+      <Link to={`/${id}`}>
+        {text} <button onClick={onBtnClick}>DEL</button>
+      </Link>
     </li>
   );
 }
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,22 @@
-export default () => "Detail";
+import React from "react";
+import { connect } from "react-redux";
+
+function Detail({ toDo }) {
+  return (
+    <>
+      <h1>{toDo?.text}</h1>
+      <h5>Created at: {toDo?.id}</h5>
+    </>
+  );
+}
+
+function mapStateToProps(state, ownProps) {
+  const {
+    match: {
+      params: { id }
+    }
+  } = ownProps;
+  return { toDo: state.find(toDo => toDo.id === parseInt(id)) };
+}
+
+export default connect(mapStateToProps)(Detail);
