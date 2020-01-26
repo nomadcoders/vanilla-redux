7102e62ed64133e7088ee44dcc9e897ff917587f
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-01-26 19:25:18 +0900

    #2.3 Delete To Do part Two

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const reducer = (state = [], action) => {
     case ADD_TODO:
       return [{ text: action.text, id: Date.now() }, ...state];
     case DELETE_TODO:
-      return [];
+      return state.filter(toDo => toDo.id !== action.id);
     default:
       return state;
   }
@@ -40,7 +40,7 @@ const dispatchAddToDo = text => {
 };
 
 const dispatchDeleteToDo = e => {
-  const id = e.target.parentNode.id;
+  const id = parseInt(e.target.parentNode.id);
   store.dispatch(deleteToDo(id));
 };
 
