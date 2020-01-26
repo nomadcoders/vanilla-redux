fa52dbffb929b9373e166e64bd7b1360ff579e71
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-01-26 18:17:39 +0900

    #1.1 Store and Reducer

--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+const countModifier = (count = 0) => {
+  return count;
 };
 
-const handleAdd = () => {
-  count = count + 1;
-  updateText();
-};
+const countStore = createStore(countModifier);
 
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+console.log(countStore.getState());
