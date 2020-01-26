794f2a3eb7d169de7ca240b163e481a22653f7bd
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-01-26 19:30:23 +0900

    #2.4 Conclusions

--- src/index.js
+++ src/index.js
@@ -23,9 +23,11 @@ const deleteToDo = id => {
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      return [{ text: action.text, id: Date.now() }, ...state];
+      const newToDoObj = { text: action.text, id: Date.now() };
+      return [newToDoObj, ...state];
     case DELETE_TODO:
-      return state.filter(toDo => toDo.id !== action.id);
+      const cleaned = state.filter(toDo => toDo.id !== action.id);
+      return cleaned;
     default:
       return state;
   }
