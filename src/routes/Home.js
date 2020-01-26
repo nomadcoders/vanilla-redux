456d1230117f29d755ef9a8e8e2265c8b72ec5f1
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-01-26 19:49:16 +0900

    #3.0 Setup

---
+++ src/routes/Home.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+function Home() {
+  const [text, setText] = useState("");
+  function onChange(e) {
+    setText(e.target.value);
+  }
+  function onSubmit(e) {
+    e.preventDefault();
+    setText("");
+  }
+  return (
+    <>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <button>Add</button>
+      </form>
+      <ul></ul>
+    </>
+  );
+}
+
+export default Home;
