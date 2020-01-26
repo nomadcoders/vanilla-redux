2065c3813158582509f41c299f55f39e02bc9450
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-01-26 20:10:04 +0900

    #3.3 mapDispatchToProps

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
+import { actionCreators } from "../store";
 
-function Home({ toDos }) {
+function Home({ toDos, addToDo }) {
   const [text, setText] = useState("");
   function onChange(e) {
     setText(e.target.value);
   }
   function onSubmit(e) {
     e.preventDefault();
+    addToDo(text);
     setText("");
   }
   return (
@@ -26,4 +28,10 @@ function mapStateToProps(state) {
   return { toDos: state };
 }
 
-export default connect(mapStateToProps)(Home);
+function mapDispatchToProps(dispatch) {
+  return {
+    addToDo: text => dispatch(actionCreators.addToDo(text))
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- src/store.js
+++ src/store.js
@@ -3,14 +3,14 @@ import { createStore } from "redux";
 const ADD = "ADD";
 const DELETE = "DELETE";
 
-export const addToDo = text => {
+const addToDo = text => {
   return {
     type: ADD,
     text
   };
 };
 
-export const deleteToDo = id => {
+const deleteToDo = id => {
   return {
     type: DELETE,
     id
@@ -29,5 +29,10 @@ const reducer = (state = [], action) => {
 };
 
 const store = createStore(reducer);
+
+export const actionCreators = {
+  addToDo,
+  deleteToDo
+};
 
 export default store;
