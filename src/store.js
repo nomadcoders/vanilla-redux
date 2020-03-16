596768ef9829fb93049ddd3c94a34b0f5bd62370
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-03-16 18:33:20 +0900

    #4.3 configureStore

--- src/store.js
+++ src/store.js
@@ -1,19 +1,7 @@
-import { createStore } from "redux";
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { createAction, createReducer, configureStore } from "@reduxjs/toolkit";
 
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
-
-/* const reducer = (state = [], action) => {
-  switch (action.type) {  
-    case addToDo.type:
-      return [{ text: action.payload, id: Date.now() }, ...state];
-    case deleteToDo.type:
-      return state.filter(toDo => toDo.id !== action.payload);
-    default:
-      return state;
-  }
-}; */
 
 const reducer = createReducer([], {
   [addToDo]: (state, action) => {
@@ -23,7 +11,7 @@ const reducer = createReducer([], {
     state.filter(toDo => toDo.id !== action.payload)
 });
 
-const store = createStore(reducer);
+const store = configureStore({ reducer });
 
 export const actionCreators = {
   addToDo,
