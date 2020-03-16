8b6b86867351967b50f3fe1cfba221a2ca5a5439
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-03-16 18:06:33 +0900

    #4.1 createAction

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -6,9 +6,8 @@ import { Link } from "react-router-dom";
 function ToDo({ text, onBtnClick, id }) {
   return (
     <li>
-      <Link to={`/${id}`}>
-        {text} <button onClick={onBtnClick}>DEL</button>
-      </Link>
+      <Link to={`/${id}`}>{text}</Link>
+      <button onClick={onBtnClick}>DEL</button>
     </li>
   );
 }
--- src/store.js
+++ src/store.js
@@ -1,28 +1,15 @@
 import { createStore } from "redux";
+import { createAction } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
-
-const addToDo = text => {
-  return {
-    type: ADD,
-    text
-  };
-};
-
-const deleteToDo = id => {
-  return {
-    type: DELETE,
-    id: parseInt(id)
-  };
-};
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
 
 const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter(toDo => toDo.id !== action.id);
+    case addToDo.type:
+      return [{ text: action.payload, id: Date.now() }, ...state];
+    case deleteToDo.type:
+      return state.filter(toDo => toDo.id !== action.payload);
     default:
       return state;
   }
